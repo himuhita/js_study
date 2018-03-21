60481f819e354a53b8cc16793deadea49f11fb6f
himu hita <himu@gmail.com>
2018-03-21 15:53:13 +0600

    Add initial project

---
+++ raw/js/t1.js
@@ -0,0 +1,14 @@
+function lOn(){
+        document.getElementById('myImage').src='/home/himu/js/raw/img/pic_bulbon.gif';
+        }
+
+function lOff(){
+        document.getElementById('myImage').src='/home/himu/js/raw/img/pic_bulboff.gif';
+        }
+        
+function INNERhtml(){
+        document.getElementById('head2').innerHTML ='Hello world';
+        }
+        
+function WriteAlert(){
+        }
---
+++ t1.js
@@ -0,0 +1,9 @@
+/*
+const pi = 3.19164;
+console.log(pi);
+*/
+var n= 1, result;
+var remainder = n%2;
+
+result = (0 == remainder) ? "even" : "odd";
+console.log("This number is ",result);
