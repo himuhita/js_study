86305332c43a6cff0b359a579a2d99b6ce8a861e
himu hita <himu@gmail.com>
2018-03-21 16:57:49 +0600

    Change t1.js files variable value.

--- t1.js
+++ t1.js
@@ -2,9 +2,9 @@
 const pi = 3.19164;
 console.log(pi);
 */
-var n= 1, result;
+var n= 112, result;
 var remainder = n%2;
 
 result = (0 == remainder) ? "even" : "odd";
 console.log("This number is ",result);
-// result show done 
+// result show done
